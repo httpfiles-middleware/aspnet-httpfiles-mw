ef55fd6a4567d98b513c5a5a9b68f8e7bb7454ce
Martin Flores <martinflores@microsoft.com>
2025-09-20 23:50:08 -0600

    code cleanup

--- tests/HttpFilesMW.Core.IntegrationTests/Controllers/TestController.cs
+++ tests/HttpFilesMW.Core.IntegrationTests/Controllers/TestController.cs
@@ -15,19 +15,19 @@ public class TestController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        return Ok(new { Message = "Hello World" });
+        return this.Ok(new { Message = "Hello World" });
     }
 
     [HttpGet("{id}")]
     public IActionResult GetById(int id)
     {
-        return Ok(new { Id = id, Name = $"Item {id}" });
+        return this.Ok(new { Id = id, Name = $"Item {id}" });
     }
 
     [HttpPost]
     public IActionResult Post([FromBody] CreateItemRequest request)
     {
-        return Ok(new { Id = 1, request.Name });
+        return this.Ok(new { Id = 1, request.Name });
     }
 }
 
